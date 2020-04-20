493159e47cda6f3c22f4bd3995d6d1fb03f2fceb
Lance <491799643@qq.com>
2020-04-20 16:46:43 +0800

    将代码重构到抽象的父类骨架中

--- src/main/java/com/github/hcsp/polymorphism/清炒菜心.java
+++ src/main/java/com/github/hcsp/polymorphism/清炒菜心.java
@@ -14,7 +14,7 @@ public class 清炒菜心 extends 菜 {
 
     @Override
     public void 放佐料() {
-        System.out.println("放入酱油");
+        System.out.println("放酱油");
         System.out.println("放盐");
     }
 
