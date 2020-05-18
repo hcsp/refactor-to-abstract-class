512abba8b2f95108f32045095bc4115817087cab
Administrator <756433556@qq.com>
2020-05-18 23:43:59 +0800

    将代码重构到抽象的父类骨架中

--- src/main/java/com/github/hcsp/polymorphism/清炒菜心.java
+++ src/main/java/com/github/hcsp/polymorphism/清炒菜心.java
@@ -4,7 +4,12 @@ public class 清炒菜心 extends 菜 {
     public 清炒菜心 () {
         super("清炒菜心");
     }
+    @Override
+    public void 开始烹饪() {
+        System.out.println("放青菜");
+        System.out.println("炒啊炒啊炒");
 
+    }
     @Override
     public void 倒油() {
         System.out.println("倒一点点油");
@@ -12,12 +17,7 @@ public class 清炒菜心 extends 菜 {
 
 
 
-    @Override
-    public void 开始烹饪() {
-        System.out.println("放青菜");
-        System.out.println("炒啊炒啊炒");
 
-    }
     @Override
     public void 放佐料() {
         System.out.println("放酱油");
--- src/main/java/com/github/hcsp/polymorphism/煎牛排.java
+++ src/main/java/com/github/hcsp/polymorphism/煎牛排.java
@@ -9,18 +9,18 @@ public class 煎牛排 extends 菜 {
     public void 洗锅() {
         System.out.println("洗煎锅");
     }
-
-    @Override
-    public void 倒油() {
-        System.out.println("倒油");
-    }
-
     @Override
     public void 开始烹饪() {
         System.out.println("放牛排");
         System.out.println("煎啊煎啊煎");
 
     }
+    @Override
+    public void 倒油() {
+        System.out.println("倒油");
+    }
+
+
     @Override
     public void 放佐料() {
         System.out.println("放胡椒粉");
