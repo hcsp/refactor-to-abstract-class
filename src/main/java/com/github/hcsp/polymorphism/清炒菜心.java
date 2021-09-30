791b62fcd1d947359ee4b593100b2dc8ade33724
GrumpyCitizenBear <1049378767@qq.com>
2021-09-30 17:22:16 +0800

    做菜

--- src/main/java/com/github/hcsp/polymorphism/清炒菜心.java
+++ src/main/java/com/github/hcsp/polymorphism/清炒菜心.java
@@ -1,18 +1,25 @@
 package com.github.hcsp.polymorphism;
 
-public class 清炒菜心 extends 菜{
+public class 清炒菜心 extends 菜 {
     @Override
-    public void 倒油(){
+    public void 倒油() {
         System.out.println("倒一丢丢油");
     }
+
     @Override
-    public void 开始烹饪(){
+    public void 开始烹饪() {
         System.out.println("放青菜");
         System.out.println("炒啊炒啊炒");
     }
 
     @Override
-    public void 出锅(){
+    public void 放佐料() {
+        System.out.println("放酱油");
+        System.out.println("放盐");
+    }
+
+    @Override
+    public void 出锅() {
         System.out.println("香喷喷的清炒青菜出锅啦");
     }
 }
