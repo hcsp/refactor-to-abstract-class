a2fcb53ebe812b8f43153ea78b6fa7eedd1c1cdb
AdFeng <3200185362@qq.com>
2021-10-12 17:57:18 +0800

    代码重构到抽象的父类骨架中

--- src/main/java/com/github/hcsp/polymorphism/西红柿炒鸡蛋.java
+++ src/main/java/com/github/hcsp/polymorphism/西红柿炒鸡蛋.java
@@ -1,6 +1,9 @@
 package com.github.hcsp.polymorphism;
 
 public class 西红柿炒鸡蛋 extends 菜{
+    public void 倒油() {
+        System.out.println("倒油");
+    }
     public void 开始烹饪() {
         System.out.println("放鸡蛋");
         System.out.println("放西红柿");
