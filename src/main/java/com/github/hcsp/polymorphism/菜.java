3b53ec5cbe339432dcee3394f5d5716934ebd7e8
AdFeng <3200185362@qq.com>
2021-10-12 17:52:38 +0800

    代码重构到抽象的父类骨架中

--- src/main/java/com/github/hcsp/polymorphism/菜.java
+++ src/main/java/com/github/hcsp/polymorphism/菜.java
@@ -9,13 +9,25 @@ public class 菜 {
         出锅();
     }
 
-    public void 洗锅() {}
+    public void 洗锅() {
+        System.out.println("洗炒锅");
+    }
 
-    public void 倒油() {}
+    public void 倒油() {
+        System.out.println("倒一点点油");
+    }
 
-    public void 开始烹饪() {}
+    public void 开始烹饪() {
+        System.out.println("放青菜");
+        System.out.println("炒啊炒啊炒");
+    }
 
-    public void 放佐料() {}
+    public void 放佐料() {
+        System.out.println("放酱油");
+        System.out.println("放盐");
+    }
 
-    public void 出锅() {}
+    public void 出锅() {
+        System.out.println("香喷喷的菜出锅啦");
+    }
 }
