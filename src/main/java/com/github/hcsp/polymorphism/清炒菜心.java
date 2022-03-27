4362c399c7f4fa2f189b221afe3efde83b1fc8d5
LiDianguang <1789681874@qq.com>
2022-03-28 02:42:55 +0800

    update

--- src/main/java/com/github/hcsp/polymorphism/清炒菜心.java
+++ src/main/java/com/github/hcsp/polymorphism/清炒菜心.java
@@ -15,7 +15,7 @@ public class 清炒菜心 extends 菜 {
         System.out.println("放青菜");
         System.out.println("炒啊炒啊炒");
     }
-
+    @Override
     public void 放佐料() {
         System.out.println("放酱油");
         super.放佐料();
