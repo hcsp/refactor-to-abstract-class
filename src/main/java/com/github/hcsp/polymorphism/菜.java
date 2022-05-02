73febf3b2147923fdb04962647fd3d9598a7b50f
AzusaRin <103942573+AzusaRin@users.noreply.github.com>
2022-05-02 16:20:52 +0800

    11 (#250)
    11 (#250)

--- src/main/java/com/github/hcsp/polymorphism/菜.java
+++ src/main/java/com/github/hcsp/polymorphism/菜.java
@@ -1,6 +1,19 @@
 package com.github.hcsp.polymorphism;
 
 public class 菜 {
+
+
+    private String guoname;
+    private String youliang;
+
+    public 菜(String guoname, String youliang, String cainame) {
+        this.guoname = guoname;
+        this.youliang = youliang;
+        this.cainame = cainame;
+    }
+
+    private String cainame;
+
     public void 做一个菜() {
         洗锅();
         倒油();
@@ -9,13 +22,23 @@ public class 菜 {
         出锅();
     }
 
-    public void 洗锅() {}
+    public void 洗锅() {
+        System.out.println("洗" + guoname + "锅");
+    }
 
-    public void 倒油() {}
+    public void 倒油() {
+        System.out.println("倒" + youliang + "油");
 
-    public void 开始烹饪() {}
+    }
 
-    public void 放佐料() {}
+    public void 开始烹饪() {
+    }
 
-    public void 出锅() {}
+    public void 放佐料() {
+        System.out.println("放盐");
+    }
+
+    public void 出锅() {
+        System.out.println("香喷喷的" + cainame + "出锅啦");
+    }
 }
